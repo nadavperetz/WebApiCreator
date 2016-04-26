0e75aab33cc83a093852e8e1df29c3cbd0b81aa5
nadavperetz <nadavperetz@gmail.com>
2016-04-26 18:23:06 -0300

    Hello World

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
@@ -14,14 +14,19 @@ db = SQLAlchemy(app)
 from models.results import Result
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def hello_world():
-    return render_template('index.html')
+    errors = []
+    results = {}
+    return render_template('index.html', errors=errors, results=results)
 
 
-@app.route('/<name>')
+@app.route('/<name>', methods=['GET', 'POST'])
 def hello_name(name):
-    return "Hello {}!".format(name)
+    if request.method == "POST":
+        return str(request.data)
+    else:
+        return 'Hello {0}'.format(name)
 
 
 if __name__ == '__main__':
